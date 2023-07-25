d9a0b8dda4c1895bf72bf72b4764dc820c4967bb
AntonyBil <beletskiy.ant@gmail.com>
2023-07-25 20:53:52 +0300

    Initial commit

---
+++ ToDoList_App/ToDoList_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList_AppApp.swift
+//  ToDoList_App
+//
+//  Created by apple on 25.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
