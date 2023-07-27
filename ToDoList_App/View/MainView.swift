085933b065e4b881edd57b74804e9e1eb1fa02f2
AntonyBil <beletskiy.ant@gmail.com>
2023-07-27 23:06:24 +0300

    Work on account UI

--- ToDoList_App/View/MainView.swift
+++ ToDoList_App/View/MainView.swift
@@ -11,17 +11,29 @@ struct MainView: View {
     @StateObject var viewModel = MainViewViewModel()
     
     var body: some View {
-        NavigationView {
             if viewModel.isSignedIn, !viewModel.currentUserId.isEmpty {
-                //signed in
-                ToDoListView()
+                accountView
             } else {
                 LoginView()
             }
         }
-        
+    
+        @ViewBuilder
+        var accountView: some View {
+            TabView {
+                ToDoListView(userId: viewModel.currentUserId)
+                    .tabItem {
+                        Label("Home", systemImage: "house")
+                    }
+                
+                ProfileView()
+                    .tabItem {
+                        Label("Profile", systemImage: "person.circle")
+                    }
+            }
+        }
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
